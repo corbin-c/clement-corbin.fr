216a8a2522ba1620b30d20f6583d2bff0ee6a3c3
Clément Corbin <corbin.clement@gmail.com>
2021-03-15 22:20:27 +0100

    404 route

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,35 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue"
+import VueRouter from "vue-router"
+import Home from "../views/Home.vue"
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('../views/About.vue')
+    path: "/about",
+    name: "About",
+    component: () => import("../views/About.vue")
   },
   {
-    path: '/contact',
-    name: 'Contact',
-    component: () => import('../views/Contact.vue')
+    path: "/contact",
+    name: "Contact",
+    component: () => import("../views/Contact.vue")
+  },
+  {
+    path: "*",
+    alias: "/404",
+    name: "404",
+    component: () => import("../views/404.vue")
   }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes
 })
